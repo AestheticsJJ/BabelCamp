388a5678631aaec3581d2571c0c4c10c99aad108
Jiahao Ji <jiahao.ji@babel.es>
2020-02-24 10:03:54 +0100

    Added css and js code

---
+++ JSBasics/scripts/main.js
@@ -0,0 +1,23 @@
+const myHeading = document.querySelector('h1');
+myHeading.textContent = 'Hello World!';
+
+var miBoton = document.querySelector('button');
+var miTitulo = document.querySelector( 'h1');
+
+function estableceNombreUsuario() {
+    var miNombre = prompt('Por favor, ingresa tu nombre.');
+    localStorage.setItem('nombre', miNombre);
+    miTitulo.textContent = 'Mozilla es fresco,' + miNombre;
+}
+
+if (!localStorage.getItem('nombre')) {
+    estableceNombreUsuario();
+}
+else {
+    var nombreAlmacenado = localStorage.getItem('nombre');
+    miTitulo.textContent = 'Mozilla is cool, ' + nombreAlmacenado;
+}
+
+miBoton.onclick = function() {
+    estableceNombreUsuario();
+}
